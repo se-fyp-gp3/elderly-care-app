7341e926551a02e85f06f4752dc7301eb18cbfb1
Choi <c5827857@gmail.com>
2026-01-20 15:42:35 +0800

    chore: remove APPWRITE_API_KEY from .env and clean up TabsLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -113,13 +113,6 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
-        name="elderly/index"
-        options={{
-          href: null,
-          title: "Elderly List",
-        }}
-      />
-      <Tabs.Screen
         name="elderly/[id]"
         options={{
           href: null,
