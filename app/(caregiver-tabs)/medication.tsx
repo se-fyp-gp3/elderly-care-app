ffef12cf257a9f95710638ea81cfd1f1edfdc9bc
Choi <c5827857@gmail.com>
2026-02-23 00:02:42 +0800

    style: add horizontal padding to statsRow for improved layout

--- app/(caregiver-tabs)/medication.tsx
+++ app/(caregiver-tabs)/medication.tsx
@@ -1510,6 +1510,7 @@ const styles = StyleSheet.create({
   statsRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+    paddingHorizontal: 12,
   },
   stat: {
     alignItems: "center",
