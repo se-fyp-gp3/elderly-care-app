f3fe717d0bdd3354d597ac69caa4f47828e519d8
Choi <c5827857@gmail.com>
2026-04-08 20:07:49 +0800

    优化代码格式，统一导入顺序

--- lib/elderly-status.ts
+++ lib/elderly-status.ts
@@ -2,13 +2,13 @@ import i18n from "@/lib/i18n";
 import { ElderlyStatus, HealthData } from "@/types/appwrite";
 import { Query } from "react-native-appwrite";
 import {
-  DATABASE_ID,
-  ELDERLY_MEDICATION_REMINDER_TABLE_ID,
-  ELDERLY_MEDICATION_TABLE_ID,
-  HEALTH_DATA_TABLE_ID,
-  MEDICATION_LOGS_TABLE_ID,
-  SCHEDULE_TABLE_ID,
-  tablesDB,
+    DATABASE_ID,
+    ELDERLY_MEDICATION_REMINDER_TABLE_ID,
+    ELDERLY_MEDICATION_TABLE_ID,
+    HEALTH_DATA_TABLE_ID,
+    MEDICATION_LOGS_TABLE_ID,
+    SCHEDULE_TABLE_ID,
+    tablesDB,
 } from "./appwrite";
 
 export interface ElderlyStatusInfo {
